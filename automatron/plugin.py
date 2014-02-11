2776c1268d26d1335d41f762277965ac8dc32af1
Ingmar Steen <iksteen@gmail.com>
2014-02-11 11:47:06 +0100

    Make PluginManager use t.p.log.

--- automatron/plugin.py
+++ automatron/plugin.py
@@ -1,4 +1,3 @@
-import sys
 from twisted.plugin import getPlugins, IPlugin
 from twisted.python import log
 import zope.interface
@@ -37,36 +36,37 @@ class PluginManager(object):
             try:
                 zope.interface.verify.verifyObject(IAutomatronPluginFactory, plugin_class)
             except (zope.interface.verify.BrokenImplementation, zope.interface.verify.BrokenMethodImplementation) as e:
-                print >>sys.stderr, 'Invalid plugin: %s' % plugin_class.__name__
-                print >>sys.stderr, e
+                log.msg('Invalid plugin: %s' % plugin_class.__name__)
+                log.err()
                 continue
             for event_interface in zope.interface.implementedBy(plugin_class):
                 if event_interface.extends(IAutomatronEventHandler):
                     try:
                         zope.interface.verify.verifyClass(event_interface, plugin_class)
-                    except zope.interface.verify.BrokenMethodImplementation as e:
-                        print >>sys.stderr, 'Invalid plugin: %s' % plugin_class
-                        print >>sys.stderr, e
+                    except zope.interface.verify.BrokenMethodImplementation:
+                        log.msg('Invalid plugin: %s' % plugin_class.name)
+                        log.err()
                         break
             else:
                 plugins.append(plugin_class(self.controller))
+                log.msg('Loaded plugin %s' % plugin_class.name)
         self.plugins = sorted(plugins, key=lambda i: i.priority)
 
     def emit(self, event, *args):
         event_interface = event.interface
         if not event_interface.extends(IAutomatronEventHandler):
-            print >>sys.stderr, 'Emitted event %s\'s interface (%s) does not extend IAutomatronEventHandler' % \
-                                (event.getName(), event_interface)
+            log.msg('Emitted event %s\'s interface (%s) does not extend IAutomatronEventHandler' %
+                    (event.getName(), event_interface))
             return
 
         if len(args) < len(event.required):
-            print >>sys.stderr, 'Emitted event %s\'s declaration requires at least %d arguments, only %d were ' \
-                                'provided.' % (event.getName(), len(event.required), len(args))
+            log.msg('Emitted event %s\'s declaration requires at least %d arguments, only %d were '
+                    'provided.' % (event.getName(), len(event.required), len(args)))
             return
 
         if len(args) > len(event.positional):
-            print >>sys.stderr, 'Emitted event %s\'s declaration requires at most %d arguments, %d were ' \
-                                'provided.' % (event.getName(), len(event.positional), len(args))
+            log.msg('Emitted event %s\'s declaration requires at most %d arguments, %d were '
+                    'provided.' % (event.getName(), len(event.positional), len(args)))
             return
 
         for plugin in self.plugins:
