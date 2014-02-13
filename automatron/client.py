eec6c8123131a34022c88866935fde8e732c6a48
Ingmar Steen <iksteen@gmail.com>
2014-02-13 09:52:30 +0100

    Fix IRC nickname/username/hostname regexp.

--- automatron/client.py
+++ automatron/client.py
@@ -8,7 +8,7 @@ from automatron.plugin import IAutomatronEventHandler
 i = random.randrange(10000)
 DEFAULT_NICKNAME = 'automatron%d,automatron%d_,automatron%d__' % (i, i, i)
 DEFAULT_REALNAME = 'Automatron %d' % i
-USER_RE = re.compile(r'(.*)?!(.*)?@(.*)')
+USER_RE = re.compile(r'(.*?)!(.*?)@(.*)')
 
 
 class Client(irc.IRCClient):
